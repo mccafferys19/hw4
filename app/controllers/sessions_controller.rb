afb9f2554949f074af82b251ea8fd554b467c09f
Stephen McCaffery <stephen.mccaffery@kellogg.northwestern.edu>
2025-05-30 19:59:57 +0000

    complete user stories 1-7

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,37 @@
 class SessionsController < ApplicationController
   def new
+    # render login form in sessions/new.html.erb
   end
 
   def create
+   # authenticate the user
+    # 1. try to find the user by their unique identifier
+    @user = User.find_by({"email" => params["email"]})
+    # 2. if the user exists -> check if they know their password
+    if @user !=nil # -- "!=nil" asks if it is not nothing
+    # 3. if they know their password -> login is successful
+      if BCrypt::Password.new(@user["password"]) == params["password"]
+       # add a cookie here -- "cookies" is a built in hash
+        session["user_id"] = @user["id"] # session --> encrypted (2-way) cookie hash
+
+        flash["notice"] = "Welcome."
+        redirect_to "/places?logged_in_user_id=#{@user["id"]}" # http is stateless, so we have to define this specific user state
+      else
+        flash["notice"] = "Incorrect Password."
+        redirect_to "/sessions"  
+      end
+    else
+    # 4. if the user doesn't exist or they don't know their password -> login fails
+      flash["notice"] = "User not found."
+      redirect_to "/sessions"  
+    end
   end
 
   def destroy
+    # logout the user
+    session["user_id"] = nil
+    flash["notice"] = "Goodbye."
+    redirect_to "/login"
   end
 end
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+   # Custom routes for session login/logout
+  get("/login", {:controller => "sessions", :action => "new"})   # Show login form
+  post("/login", {:controller => "sessions", :action => "create"})   # Handle login form submit
+  get("/logout", {:controller => "sessions", :action => "destroy"})  # Handle logout
+ 
   get("/", { :controller => "places", :action => "index" })
   resources "entries"
   resources "places"
